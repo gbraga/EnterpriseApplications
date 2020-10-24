7bdd029b862b4a4e9bc05dfefd61fab5d9d2367b
Gabriel Braga <gbraga.dev@gmail.com>
2020-10-24 15:57:59 -0300

    configuração do jwt

---
+++ src/services/NSE.Identidade.API/Extentions/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Extentions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+
+        public int ExpirationHours { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string ValidOn { get; set; }
+    }
+}
