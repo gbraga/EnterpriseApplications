5086c9ce44a686b29ea53550f04d32f04f954c0a
Gabriel Braga <gbraga.dev@gmail.com>
2020-10-24 20:03:25 -0300

    resposta customizada com boas práticas

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -16,7 +16,7 @@ namespace NSE.Identidade.API.Controllers
 {
     [ApiController]
     [Route("api/identidade")]
-    public class AuthController : Controller
+    public class AuthController : MainController
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
@@ -34,7 +34,7 @@ namespace NSE.Identidade.API.Controllers
         [HttpPost("nova-conta")]
         public async Task<ActionResult> Registrar(UsuarioRegistroViewModel usuarioRegistroViewModel)
         {
-            if (!ModelState.IsValid) return BadRequest();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
             var user = new IdentityUser
             {
@@ -47,18 +47,22 @@ namespace NSE.Identidade.API.Controllers
 
             if (result.Succeeded)
             {
-                await _signInManager.SignInAsync(user, isPersistent: false);
-                return Ok(value: await GerarJwt(usuarioRegistroViewModel.Email));
+                return CustomResponse(await GerarJwt(usuarioRegistroViewModel.Email));
             }
 
-            return BadRequest();
+            foreach (var error in result.Errors)
+            {
+                AdicionarErroProcessamento(error.Description);
+            }
+
+            return CustomResponse();
         }
 
         [HttpPost("autenticar")]
 
         public async Task<ActionResult> Login(UsuarioLoginViewModel usuarioLoginViewModel)
         {
-            if (!ModelState.IsValid) return BadRequest();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
             var result = await _signInManager.PasswordSignInAsync(
                 userName: usuarioLoginViewModel.Email,
@@ -69,10 +73,17 @@ namespace NSE.Identidade.API.Controllers
 
             if (result.Succeeded)
             {
-                return Ok(value: await GerarJwt(usuarioLoginViewModel.Email));
+                return CustomResponse(await GerarJwt(usuarioLoginViewModel.Email));
             }
 
-            return BadRequest();
+            if (result.IsLockedOut)
+            {
+                AdicionarErroProcessamento("Usuário temporariamente bloqueado por tentativas inválidas");
+                return CustomResponse();
+            }
+
+            AdicionarErroProcessamento("Usuário ou Senha incorretos");
+            return CustomResponse();
         }
 
         private async Task<UsuarioRespostaLoginViewModel> GerarJwt(string email)
